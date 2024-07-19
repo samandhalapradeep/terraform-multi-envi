715d4689f4e3f313d42983a4dc48666f11fe818c
samandhalapradeep <pradeepsamandhala7@gmail.com>
2024-07-19 21:25:06 +0530

    terraform

---
+++ tfvar/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "daws76s-state-prodd"
+key    = "multienv"
+region = "us-east-1"
+dynamodb_table = "daws76s-locking-prodd"
--- workspaces/provider.tf
+++ workspaces/provider.tf
@@ -1,9 +1,17 @@
+
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.31.0" # AWS provider version, not terraform version
     }
+  }
+
+  backend "s3" {
+    bucket = "daws76s-remote-state"
+    key    = "workspace"
+    region = "us-east-1"
+    dynamodb_table = "daws76s-locking"
   }
 }
 
